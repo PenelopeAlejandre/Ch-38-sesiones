d9549123a6375dabc9bc5e59534efb6b667ab35a
Penelope Alejandre <penelopealejandrelopez@gmail.com>
2024-02-19 13:28:15 -0600

    ci: funcion pedir nombre

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log ("Hola Ch38");
+
+//función que envíe un saludo con alert//
+
+function enviarsaludo (){
+    alert ("Hola Ch 38");
+}
+
+/**
+ * Pedir el nombre de la persona usando Promt() 
+ * Mostrar un saludo y nombre de la persona con alert
+ */
+function enviarsaludoapersona(){
+    let message= prompt ("Cual es tu nombre");
+    alert ("Hola " + message);
+}
+
